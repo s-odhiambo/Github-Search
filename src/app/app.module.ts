2f99fa0be0b7173387196a69addf5cfac8e15785
s-odhiambo <samuelangienda1998@gmail.com>
2019-11-05 10:50:20 +0300

    add git-form.component folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GitFormComponent } from './git-form/git-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitFormComponent
   ],
   imports: [
     BrowserModule,
